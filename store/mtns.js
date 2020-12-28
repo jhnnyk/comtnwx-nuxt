94744ecde2d03493ccadbe7fe30861a89ffe7beb
John Knotts <jhnnyk@gmail.com>
2020-12-28 13:12:10 -0700

    get weather forecast (with axios module)

--- store/mtns.js
+++ store/mtns.js
@@ -3,6 +3,8 @@ import { db } from '../firebase'
 export const state = () => ({
   all: [],
   currentMtn: null,
+  forecast: [],
+  forecastLoading: false,
 })
 
 export const getters = {
@@ -30,6 +32,12 @@ export const mutations = {
   setCurrentMtn: (state, mtn) => {
     state.currentMtn = mtn
   },
+  setForecast: (state, forecast) => {
+    state.forecast = forecast
+  },
+  setForecastLoading: (state, val) => {
+    state.forecastLoading = val
+  },
 }
 
 export const actions = {
@@ -49,8 +57,47 @@ export const actions = {
     commit('setMtns', mtnList)
   },
 
-  getCurrentMtn: async ({ commit, state }, slug) => {
+  getCurrentMtn: async ({ dispatch, commit, state }, slug) => {
     const currentMtn = await state.all.find((m) => m.slug === slug)
     commit('setCurrentMtn', currentMtn)
+
+    if (currentMtn) {
+      commit('setForecastLoading', true)
+      dispatch('getForecastURL', currentMtn)
+      dispatch('getForecast', currentMtn)
+    }
+  },
+
+  async getForecastURL(context, mtn) {
+    const response = await this.$axios.$get(
+      `https://api.weather.gov/points/${mtn.loc.latitude},${mtn.loc.longitude}`
+    )
+    const forecastURL = await response.properties.forecast
+
+    // update DB with forecast URL if needed
+    // ----------------------------------
+    // if the DB does need updated with a new forecastURL,
+    // the below will run and generate an error
+    // because of firebase permissions
+    // I'm leaving it like this because per the API docs at:
+    // https://weather-gov.github.io/api/general-faqs
+    // it should rarely need updating.
+    if (forecastURL !== mtn.forecastURL) {
+      const spotRef = db.collection('spots').doc(mtn.id)
+      try {
+        await spotRef.update({
+          forecastURL,
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+
+  async getForecast({ commit }, mtn) {
+    const response = await this.$axios.$get(mtn.forecastURL)
+    const forecast = await response.properties.periods
+    commit('setForecast', forecast)
+    commit('setForecastLoading', false)
   },
 }
